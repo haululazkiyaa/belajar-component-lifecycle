7115f81ea01f3460ee4ed1894b4ee0d56ab919a1
Muhammad Haulul Azkiyaa <haululazkiya@gmail.com>
2024-09-02 17:44:06 +0700

    try axios

--- src/components/Child.jsx
+++ src/components/Child.jsx
@@ -1,17 +1,46 @@
 import { useEffect, useState } from "react";
 
+import axios from "axios";
+
 const Child = () => {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/posts")
-      .then((response) => response.json())
-      .then((data) => setData(data));
+    // fetch("https://jsonplaceholder.typicode.com/posts")
+    //   .then((response) => response.json())
+    //   .then((data) => setData(data));
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => setData(response.data))
+      .catch((error) => console.log(error));
   }, []);
+
+  const handleAddNewPost = () => {
+    const newPost = {
+      id: 101,
+      title: "foo",
+      body: "bar",
+      userId: 1,
+    };
+
+    axios
+      .post("https://jsonplaceholder.typicode.com/posts", newPost)
+      .then((response) => {
+        setData([...data, response.data]);
+      })
+      .catch((error) => console.log(error));
+  };
 
   return (
     <section>
-      <h1 className="text-3xl font-bold text-center mt-10">Posts ℹ️</h1>
+      <pre>{JSON.stringify(data, null, 4)}</pre>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={handleAddNewPost}
+      >
+        Add new data
+      </button>
+      {/* <h1 className="text-3xl font-bold text-center mt-10">Posts ℹ️</h1>
       {data.length === 0 ? (
         <p className="text-center mt-10">Memuat data...</p>
       ) : (
@@ -26,7 +55,7 @@ const Child = () => {
             </div>
           ))}
         </div>
-      )}
+      )} */}
     </section>
   );
 };
